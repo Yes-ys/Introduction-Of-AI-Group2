8da5cebc032a90f1952f4fa35b8dc64640267633
hethtina <14817187+hethtina@user.noreply.gitee.com>
2025-10-31 10:14:49 +0800

    修改了hashmap类以支持节点查找，初步写了一版PqFrontier

--- WattAi/src/stud/g01/queue/PqFrontier.java
+++ WattAi/src/stud/g01/queue/PqFrontier.java
@@ -2,27 +2,73 @@ package stud.g01.queue;
 
 import core.solver.queue.Frontier;
 import core.solver.queue.Node;
-import stud.queue.QueueFrontier;
+import core.solver.queue.EvaluationType;
 import stud.g01.utils.NpuzzleHash;
-import stud.g01.problem.npuzzle.PuzzleBoard;
+import core.problem.State;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PqFrontier extends PriorityQueue<Node> implements Frontier {
 
-    private final NpuzzleHash HashBook;
+    private final NpuzzleHash hashBook;
+    private final EvaluationType type;
 
-    public PqFrontier(){
-        HashBook = new NpuzzleHash();
+    public PqFrontier(EvaluationType type) {
+        super(Node.evaluator(type));
+        this.type = type;
+        this.hashBook = new NpuzzleHash();
     }
 
     @Override
     public boolean contains(Node node) {
-        return HashBook.check(node.getState().toString());
+        // 使用正确的方法名
+        return hashBook.contains(node.getState().toString());
     }
 
     @Override
     public boolean offer(Node node) {
-        return false;
+        String stateStr = node.getState().toString();
+        int currentCost = node.getPathCost();
+
+        // 使用 checkAndUpdate 方法检查并更新哈希表
+        boolean shouldSkip = hashBook.checkAndUpdate(stateStr, currentCost);
+
+        if (shouldSkip) {
+            // 已存在更优路径，跳过此节点
+            return false;
+        }
+
+        // 如果队列中已存在相同状态的节点，需要移除旧的
+        removeExistingNode(node.getState());
+
+        // 插入新节点
+        return super.offer(node);
     }
+
+    /**
+     * 移除队列中指定状态的节点
+     */
+    private void removeExistingNode(State state) {
+        String targetStateStr = state.toString();
+        this.removeIf(currentNode ->
+                currentNode.getState().toString().equals(targetStateStr)
+        );
+    }
+
+    /**
+     * 获取评估类型
+     */
+    public EvaluationType getEvaluationType() {
+        return type;
+    }
+//调试
+//    @Override
+//    public String toString() {
+//        return "PqFrontier{" +
+//                "type=" + type +
+//                ", size=" + this.size() +
+//                ", exploredStates=" + hashBook.size() +
+//                '}';
+//    }
 }
